dcda5383cab9a102a8f3873d98999246d892edae
Luis Heshiki <luisheshiki22@gmail.com>
2020-11-16 14:23:07 +0900

    done

--- okinawatest3.js
+++ okinawatest3.js
@@ -1,1 +1,8 @@
-console.log('Luis')
+
+if (document.querySelector('.form-control').textContent == '滝') 
+{document.querySelector('#search').href = 'okinawatest4.html'}
+else if (document.querySelector('.form-control').textContent == '食べ物')
+	{document.querySelector('#search').href = 'okinawatest5.html'}
+
+
+
